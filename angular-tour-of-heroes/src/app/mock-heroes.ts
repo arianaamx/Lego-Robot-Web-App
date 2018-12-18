e2e3ec925cf8adc4d43999aae9444d32464c1a19
arianaamx <ariana.milasincic@gmail.com>
2018-12-18 18:02:12 +0100

    Tabs

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Mr. Lucija' },
+  { id: 13, name: 'Miss. Maja' },
+  { id: 14, name: 'Mr. Magneto'}
+
+];
